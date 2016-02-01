c221c1cc488a021d564a8c28ec9b20e1b02f0675
bleffler <ben@wylei.com>
2016-02-01 15:04:12 -0500

    Add interval support  - Update README, example.js, and package.json as well

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Long timeouts
 
-Long timeout makes it possible to have a timeout that is longer than 24.8 days (2^31-1 milliseconds).
+Long timeout makes it possible to have a timeout or interval that is longer than 24.8 days (2^31-1 milliseconds).
 
 
 ## Usage
@@ -17,6 +17,16 @@ var timeout = lt.setTimeout(function() {
 lt.clearTimeout(timeout)
 ```
 
+```js
+var lt = require('longtimeout')
+
+var interval = lt.setInterval(function() {
+  console.log('every 30 days')
+}, 1000 * 60 * 60 * 24 * 30)
+
+// Clear it
+lt.clearInterval(interval);
+```
 
 ## Install
 
--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var TIMEOUT_MAX = 2147483647; // 2^31-1
 exports.setTimeout = function(listener, after) {
   return new Timeout(listener, after)
 }
+exports.setInterval = function(listener, after) {
+  return new Interval(listener, after)
+}
 exports.clearTimeout = function(timer) {
   if (timer) timer.close()
 }
+exports.clearInterval = exports.clearTimeout
 
 exports.Timeout = Timeout
+exports.Interval = Interval
 
 function Timeout(listener, after) {
   this.listener = listener
@@ -30,4 +35,31 @@ Timeout.prototype.start = function() {
 
 Timeout.prototype.close = function() {
   clearTimeout(this.timeout)
+}
+
+function Interval(listener, after) {
+  this.listener = listener
+  this.after = this.timeLeft = after
+  this.start()
+}
+
+Interval.prototype.start = function() {
+  var self = this
+
+  if (this.timeLeft <= TIMEOUT_MAX) {
+    this.timeout = setTimeout(function() {
+      self.listener()
+      self.timeLeft = self.after
+      self.start()
+    }, this.timeLeft)
+  } else {
+    this.timeout = setTimeout(function() {
+      self.timeLeft -= TIMEOUT_MAX
+      self.start()
+    }, TIMEOUT_MAX)
+  }
+}
+
+Interval.prototype.close = function() {
+  Timeout.prototype.close.apply(this, arguments);
 }
